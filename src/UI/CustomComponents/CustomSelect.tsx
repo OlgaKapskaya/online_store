eec6276154273fc085a8b308d7b535a49b0ca52a
OlgaKapskaya <olikbuko@gmail.com>
2022-11-02 16:46:47 +0300

    add custom select component

---
+++ src/UI/CustomComponents/CustomSelect.tsx
@@ -0,0 +1,78 @@
+import React, {useState, KeyboardEvent, useEffect} from "react";
+import {OptionsSelectType} from '../../BLL/types';
+import s from './CustomSelect.module.css'
+import img from './chevron-down.svg'
+
+
+type SelectProps = {
+    value: string
+    options: OptionsSelectType[]
+    onChange: (value: string) => void
+}
+export const CustomSelect = (props: SelectProps) => {
+    const [isVisible, setIsVisible] = useState<boolean>(false)
+    const [hoverElement, setHoverElement] = useState<string>(props.value)
+    useEffect( () => {
+        setHoverElement(props.value)
+    }, [props, props.value])
+    const onClickHandler = () => {
+        setIsVisible(!isVisible)
+    }
+    const onChangeSelectItem = (value: string) => {
+        props.onChange(value)
+        setIsVisible(false)
+    }
+    const onKeyUpItemHandler = (event: KeyboardEvent<HTMLDivElement>) => {
+        for (let i = 0; i < props.options.length; i++) {
+            if (hoverElement === props.options[i].value) {
+                if (props.options[i + 1] || props.options[i - 1]) {
+                    if (event.key === 'ArrowDown') {
+                        props.onChange(props.options[i + 1].value);
+                        break
+                    }
+                    if (event.key === 'ArrowUp') {
+                        props.onChange(props.options[i - 1].value);
+                        break
+                    }
+                    if (event.key === 'Enter') {
+                        props.onChange(props.options[i].value);
+                        setIsVisible(false)
+                        break
+                    }
+                }
+            } else props.onChange(props.options[0].value)
+        }
+
+    }
+
+    return (
+        <div className={s.container}>
+            <div className={s.select}
+                 onClick={onClickHandler}
+                 tabIndex={1}
+                 onKeyUp={onKeyUpItemHandler}
+            >
+                <h5>{props.value}</h5>
+                <img src={img} className={isVisible ? s.imgUp : s.imgDown} alt={'arrow'}/>
+            </div>
+            <div className={s.optionsContainer}>
+            {isVisible && props.options.map(elem => {
+                const itemClass = s.options + ' ' + (elem.value === hoverElement ? s.selected : "")
+                const onMouseEnterHandler = () => {
+                    setHoverElement(elem.value)
+                }
+
+                return (
+                    <div className={itemClass}
+                         onMouseEnter={onMouseEnterHandler}
+                         key={elem.id}
+                         onClick={() => onChangeSelectItem(elem.value)}>
+
+                        {elem.value}
+                    </div>
+                )
+            })}
+        </div>
+        </div>
+    )
+}
--- src/UI/Navigation/SearchMenu/SearchMenu.tsx
+++ src/UI/Navigation/SearchMenu/SearchMenu.tsx
@@ -1,21 +1,25 @@
-import React, {ChangeEvent} from "react";
+import React, {ChangeEvent, useState} from "react";
 import s from './SearchMenu.module.css'
 import {TextField} from "@material-ui/core";
 import {OptionsSelectType} from "../../../BLL/types";
+import {CustomSelect} from "../../CustomComponents/CustomSelect";
 
 type SearchMenuPropsType = {
     onSortedProductData: (sortInfo: string) => void
 }
 export const SearchMenu = (props: SearchMenuPropsType) => {
+    const [value, setValue] = useState('Без сортировки')
     let optionsToSelect: OptionsSelectType[] = [
-        // {id: '0', value: 'Умолчанию'},
+        // {id: '0', value: 'Без сортировки'},
         {id: '1', value: 'Названию (от А до Я)'},
         {id: '2', value: 'Названию (от Я до А)'},
         {id: '3', value: 'Стоимости (сначала дешевые)'},
         {id: '4', value: 'Стоимости (сначала дорогие)'},
     ]
-    const onChangeSelectHandler = (event: ChangeEvent<HTMLSelectElement>) => {
-        props.onSortedProductData(event.currentTarget.value)
+    const onChangeSelectHandler = (value: string) => {
+        setValue(value)
+        let index = optionsToSelect.find(elem=>elem.value===value)!.id
+        props.onSortedProductData(index)
     }
     return (
         <div className={s.MenuContainer}>
@@ -23,11 +27,12 @@ export const SearchMenu = (props: SearchMenuPropsType) => {
                            label={'Введите название'}
                            size={'small'}
                            variant={'outlined'}/>
-            <div>
+            <div className={s.selectContainer}>
                 <span> Сортировать по: </span>
-                <select onChange={onChangeSelectHandler}>
-                    {optionsToSelect.map( elem => <option value={elem.id} key={elem.id}>{elem.value}</option>)}
-                </select>
+                <CustomSelect value={value} options={optionsToSelect} onChange={onChangeSelectHandler}/>
+                {/*<select onChange={onChangeSelectHandler}>*/}
+                {/*    {optionsToSelect.map( elem => <option value={elem.id} key={elem.id}>{elem.value}</option>)}*/}
+                {/*</select>*/}
             </div>
 
         </div>
